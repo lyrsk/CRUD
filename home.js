487979c95bb596e8ad58db1c88ad466a3408d242
Eliana <crisely.cye@gmail.com>
2023-02-17 16:44:04 -0300

    Modulos

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require("express"); 
 const app = express();
-const fs = require("fs");
+const fs = require("fs"); 
 const port = 3000;
 
 app.get("/", (req, res) => {
@@ -12,7 +12,7 @@ app.get("/create", (req, res) => {
 });
 
 app.get("/read", (req, res) => {
-    const personajes = fs.readFileSync(__dirname + "/data/personajes.json");
+    const personajes = fs.readFileSync(__dirname + "/src/data/personajes.json");
     res.json(JSON.parse(personajes));
 });
 
