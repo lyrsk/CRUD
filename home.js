60769cfeb46ed7c6f27ebd7591585e8bb088e252
Eliana <crisely.cye@gmail.com>
2023-02-21 18:06:00 -0300

    Cambios

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express(); 
 const port = 3000;
 
-app.use(require("./src/routes/personajesRoutes"));
+app.use(require("./server/routes/personajesRoutes"));
 
 
 
