747656a838b0a971e8c5952b9fcab62786fbc01b
Eliana <crisely.cye@gmail.com>
2023-02-27 20:39:31 -0300

    Cambios

--- home.js
+++ home.js
@@ -2,31 +2,9 @@ const express = require("express");
 const app = express(); 
 const port = 3000;
 
+app.use(express.json());
+
 app.use(require("./server/routes/personajesRoutes"));
-
-
-
-/*
-app.get("/", (req, res) => {
-    res.send("Test")
-});
-
-app.get("/create", (req, res) => {
-
-});
-
-app.get("/read", (req, res) => {
-    const personajes = fs.readFileSync(__dirname + "/src/data/personajes.json");
-    res.json(JSON.parse(personajes));
-});
-
-app.get("/update", (req, res) => {
-
-});
-
-app.get("/delete", (req, res) => {
-
-}); */
 
 app.listen(port, () => {
     console.log(`Servidor en puerto ${port}`)
--- server/routes/personajesRoutes.js
+++ server/routes/personajesRoutes.js
@@ -4,10 +4,25 @@ const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 
-const traerPersonajes =JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/personajes.json")));
+const traerPersonajes = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/personajes.json")));
 
 router.get("/", (req, res) => {
     res.send(traerPersonajes);
+});
+
+router.post("/create", (req, res) => {
+    const id = traerPersonajes.length;
+    const nuevoPersonaje = { 
+        id: id + 1,
+        ...req.body,
+    }; 
+    traerPersonajes.push(nuevoPersonaje); 
+    fs.writeFileSync(
+        path.resolve(__dirname, "..data/personajes.json"),  // Guarda el objeto en el archivo
+        JSON.stringify(traerPersonajes, null, " ")  // Convierte el objeto javascript en objeto json (?
+    );
+    console.log(traerPersonajes);
+    res.send("Personaje guardado")
 });
 
 module.exports = router;
