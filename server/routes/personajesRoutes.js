6bc00a48d46b58051a7ef19c4b548225f09d12c4
Eliana <crisely.cye@gmail.com>
2023-02-28 00:53:05 -0300

    Cambios

--- server/routes/personajesRoutes.js
+++ server/routes/personajesRoutes.js
@@ -7,21 +7,27 @@ const fs = require("fs");
 const traerPersonajes = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/personajes.json")));
 
 router.get("/", (req, res) => {
-    res.send(traerPersonajes);
+    res.send(traerPersonajes)
 });
-
+/*
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    const personaje = traerPersonajes.find((personaje) => personaje.id == id);
+    res.send(personaje)
+});
+*/
 router.post("/create", (req, res) => {
     const id = traerPersonajes.length;
     const nuevoPersonaje = { 
         id: id + 1,
-        ...req.body,
+        ...req.body
     }; 
     traerPersonajes.push(nuevoPersonaje); 
     fs.writeFileSync(
         path.resolve(__dirname, "..data/personajes.json"),  // Guarda el objeto en el archivo
         JSON.stringify(traerPersonajes, null, " ")  // Convierte el objeto javascript en objeto json (?
     );
-    console.log(traerPersonajes);
+
     res.send("Personaje guardado")
 });
 
