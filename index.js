2bafeb9a104738411fbfa068fa7e3fc66b86ca8a
Eliana <crisely.cye@gmail.com>
2023-03-03 02:45:11 -0300

    Cambios

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import {router} from './server/routes/personajesRoutes.js';
 import express from 'express';
-
-const app = express();
-
-app.use(express.json());
-
-app.use(router);
+import {router} from './server/routes/personajesRoutes.js';
 
 const port = 3000;
+const app = express();
+
+app.use(express.json()); // Middleware
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Servidor en http://localhost:${port}`)
